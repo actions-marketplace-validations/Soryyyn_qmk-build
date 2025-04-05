e37729d2f98d78ae90d01e0dc391c10ec0605009
Soryn Bächli <soryn.baechli@gmail.com>
2025-04-05 11:16:40 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,4 +8,4 @@ ln -s "$PWD/$KEYMAP_PATH" "/qmk_firmware/$KEYMAP_PATH"
 qmk compile -kb "$KEYBOARD" -km "$KEYMAP"
 rm -rf build
 mkdir ./build
-find /qmk_firmware/.build -type f \( -name "*.hex" -o -name "*.bin" -o -name "*.uf2" \ \) -exec cp {} ./build \;
+find /qmk_firmware/.build -type f \( -name "*.hex" -o -name "*.bin" -o -name "*.uf2" \) -exec cp {} ./build \;
